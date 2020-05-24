5c52648b400df6fb94ba3dd32ae8970c91c8a3a1
madhumithabaskar95 <madhumithabaskar95@gmail.com>
2020-05-24 12:59:58 -0500

    Initial Commit

---
+++ listen2us-core/src/main/java/com/listen2uscore/listen2uscore/Listen2usCoreApplication.java
@@ -0,0 +1,13 @@
+package com.listen2uscore.listen2uscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Listen2usCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Listen2usCoreApplication.class, args);
+	}
+
+}
---
+++ listen2us-core/src/test/java/com/listen2uscore/listen2uscore/Listen2usCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listen2uscore.listen2uscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Listen2usCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
